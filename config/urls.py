6e39f1dd8f04b7afd05f5514d0b99af73df9e108
goemon024 <59425119+goemon024@users.noreply.github.com>
2024-09-17 16:43:40 +0900

    feature/registration, feature/dev-batch, feature/responsive (#6)
    * adding document folder
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * error correcting. attribute error
    
    * commit
    
    * 1st. responsive-design
    
    * 2nd
    
    * 3rd. responsive home.html
    
    * 4th.responsive-design.end
    
    * Feature/registration (#5)
    
    * commit
    
    * 1st. batch file : guest update
    
    * 2nd. batch MySQL backup at dropbox
    
    * slight change
    
    * slight change
    
    * slight change
    
    * slight change
    
    * slight change
    
    * slight change
    
    * slight change
    
    * slight change
    
    * slight change
    
    * adding password reset feature
    
    * slight change
    
    ---------
    
    Co-authored-by: Hiroto Yoshii <hirotrics@gmail.com>
    
    ---------
    
    Co-authored-by: Hiroto Yoshii <hirotrics@gmail.com>

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.urls import path,include
-from .views import CustomTemplateView
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('wlist/', include('wlist.urls')),
     # path('',TemplateView.as_view(template_name='home.html'), name='home'),
-    path('', CustomTemplateView.as_view(template_name='home.html'), name='home'),
+    path('', HomeView.as_view(template_name='home.html'), name='home'),
 
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
