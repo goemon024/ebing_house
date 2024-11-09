d8a133f1e3da90fc9da76f29c2860d48505d073e
goemon024 <hirotorics@gmail.com>
2024-11-09 10:14:33 +0900

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,8 @@ IS_HEROKU = os.getenv('DYNO') is not None
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.getenv('DEBUG')=='True'
+DEBUG = True
+# DEBUG = os.getenv('DEBUG')=='True'
 
 ALLOWED_HOSTS = [os.getenv('HEROKU_APP_COM'),'127.0.0.1', 'localhost',
                  '172.25.139.129']
