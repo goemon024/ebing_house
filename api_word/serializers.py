797afb2597162fd1d7a7cd3db42a7f848714ef47
goemon024 <hirotorics@gmail.com>
2024-11-13 20:14:03 +0900

    interval

---
+++ api_word/serializers.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from wlist.models import WordsModel
+
+class WordsSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = WordsModel
+        fields = ('user','word','mean1','mean2','reg_date','img')
+        extra_kwargs = {'user':{ 'read_only':True}}
+
+
+
+# class MemoModel(models.Model):
+#     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+#     memo = models.TextField()
+#     reg_date = models.DateField(blank=True,null=True)
+           
+#     def __str__(self):
+#         if len(self.memo) > 50:
+#             return self.memo[:50]
+#         else:
+#             return self.memo
+    
+#     def save(self, *args, **kwargs):
+#         user1 = kwargs.pop('user', None)
+#         if user1:
+#             self.user = user1
+        
+#         if not self.pk:
+#             self.reg_date = timezone.now()    
+#         else:  # 更新時
+#             original = MemoModel.objects.get(pk=self.pk)
+#             self.user = original.user
+#             self.reg_date = original.reg_date
+
+#         super(MemoModel, self).save(*args, **kwargs)
