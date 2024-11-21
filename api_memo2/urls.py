6fbba56c93962268587892456cf967fa16700ba5
goemon024 <hirotorics@gmail.com>
2024-11-21 21:50:49 +0900

    develop

--- api_memo2/urls.py
+++ api_memo2/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import Memo2ViewSet
 
 app_name = 'api_memo2'
 
 router = DefaultRouter()
+router.register(r'memo2', Memo2ViewSet, basename='memo2')
 
 urlpatterns = [
     path('',include(router.urls))
