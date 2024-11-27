5e490d49e2ce46932099f0f84d7d2fd8ba729a3a
goemon024 <hirotorics@gmail.com>
2024-11-28 01:44:35 +0900

    develop

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.urls import path,include
 from .views import HomeView
-from rest_framework.authtoken import views
+
+# from rest_framework.authtoken import views
+from .views import CustomAuthToken
 
 from django.conf.urls.static import static
 from django.conf import settings
+
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,11 +20,14 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     
-    path('authen/', views.obtain_auth_token),  # rest_frameworkの標準
+    # path('authen/', views.obtain_auth_token),  # rest_frameworkの標準
+    path('authen/', CustomAuthToken.as_view()), 
     path('api_user/',include('api_user.urls')),
     path('api_word/',include('api_word.urls')),
     path('api_memo1/',include('api_memo1.urls')),
     path('api_memo2/',include('api_memo2.urls')),
+    
+    path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
 
 ]
 
