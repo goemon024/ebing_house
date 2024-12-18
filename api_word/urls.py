47622eca5ca1b5e04bb5a5552885b12d3fd80a93
goemon024 <hirotorics@gmail.com>
2024-12-18 21:29:55 +0900

    almost complete pagenation and checklist

--- api_word/urls.py
+++ api_word/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WordsViewSet
+from .views import WordsViewSet,PageWordsViewSet
 
 app_name = 'api_word'
 
 router = DefaultRouter()
 router.register(r'word', WordsViewSet, basename='word')
+router.register(r'pageWord', PageWordsViewSet, basename='PageWord')
+
 
 urlpatterns = [
     path('',include(router.urls))
