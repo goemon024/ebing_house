a4e5106dee833aaf2ade48af007f782b56b95b88
goemon024 <hirotorics@gmail.com>
2025-01-23 17:31:06 +0900

    modifying DataContext and build

--- config/urls.py
+++ config/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     
     # path('react_index/', index, name='index'),
     
-    ##### 本番環境用 #####
+    #### 本番環境用 #####
     path('', TemplateView.as_view(template_name='index.html'), name='react_index'),
     path('media/<path:path>', serve_media, name='serve_media'),
     path('<path:path>', TemplateView.as_view(template_name='index.html')),  # その他のReact用ルート
